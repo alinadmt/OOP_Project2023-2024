88132a74e49692e50cc263fb5af18b4c5a2fb498
Alina Dumitrascu <dumitrascualina20@stud.ase.ro>
2023-11-21 18:12:46 +0200

    Adding the rest of the classes

--- OOP_Project2023-2024____/OOP_Project2023-2024____/Source.cpp
+++ OOP_Project2023-2024____/OOP_Project2023-2024____/Source.cpp
@@ -4,14 +4,34 @@ using namespace std;
 
 
 class Location {
+	const int noSeats;
+	int rows;
+	string zones;
+	int* seatsPerRow;
+	string locationName;
 
 };
-
 
 class Event {
+	string name;
+	string date;
+	string time;
+	string type;
+	const int maxTickets;
+	static int ticketsSold;
+	int eventID;
+
 
 };
 
+class Ticket {
+	int ticketID;
+	string ticketType;
+	int row;
+	int seatNo;
+	bool isValid;
+
+};
 
 
 
